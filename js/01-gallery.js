1aaf3c1d2d824e7e334062d8501404fd47681fef
penadine <105946746+penadine@users.noreply.github.com>
2023-07-06 17:25:57 +0300

    Add files via upload

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,27 +5,40 @@ console.log(galleryItems);
 
 const galleryEl = document.querySelector('.gallery');
 
-galleryItems.forEach((item) => {
+// galleryItems.forEach((item) => {
     
+//     const listItem = document.createElement('li');
+//     listItem.classList.add('gallery__item');
+  
+//     const link = document.createElement('a');
+//     link.classList.add('gallery__link');
+//     link.href = item.original;
+  
+//     link.addEventListener('click', (event) => {
+//       event.preventDefault();
+//     });
+  
+//     const image = document.createElement('img');
+//     image.classList.add('gallery__image');
+//     image.src = item.preview;
+//     image.alt = item.description;
+//     image.setAttribute('data-source', item.original);
+  
+//     link.appendChild(image);
+//     listItem.appendChild(link);
+  
+//     galleryEl.appendChild(listItem);
+//   });
+
+  galleryItems.forEach((item) => {
     const listItem = document.createElement('li');
     listItem.classList.add('gallery__item');
   
-    const link = document.createElement('a');
-    link.classList.add('gallery__link');
-    link.href = item.original;
-  
-    link.addEventListener('click', (event) => {
-      event.preventDefault();
-    });
-  
-    const image = document.createElement('img');
-    image.classList.add('gallery__image');
-    image.src = item.preview;
-    image.alt = item.description;
-    image.setAttribute('data-source', item.original);
-  
-    link.appendChild(image);
-    listItem.appendChild(link);
+    listItem.innerHTML = `
+      <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" data-source="${item.original}">
+      </a>
+    `;
   
     galleryEl.appendChild(listItem);
   });
@@ -35,12 +48,32 @@ galleryItems.forEach((item) => {
     event.preventDefault();
   
     if (event.target.tagName === 'IMG') {
+      const sourceUrl = event.target.dataset.source;
   
-        const sourceUrl = event.target.dataset.source;
-
-        const instance = basicLightbox.create(`
-          <img src="${sourceUrl}" alt="" />
-        `);
-        instance.show();
+      const instance = basicLightbox.create(`
+        <img src="${sourceUrl}" alt="" />
+      `, {
+        onShow: (instance) => {
+         
+          console.log('Lightbox shown');
+        },
+        onClose: (instance) => {
+         
+          console.log('Lightbox closed');
+        }
+      });
+  
+      instance.show();
+      instance.onShow();
+      
+      instance.onClose = () => {
+        galleryEl.removeEventListener('click', eventListener);
+      };
     }
   });
+  
+  
+  
+  
+  
+  
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,21 +5,34 @@ console.log(galleryItems);
 
 const galleryEl = document.querySelector('.gallery');
 
+// galleryItems.forEach((item) => {
+//   const listItem = document.createElement('li');
+//   listItem.classList.add('gallery__item');
+
+//   const link = document.createElement('a');
+//   link.classList.add('gallery__link');
+//   link.href = item.original;
+
+//   const image = document.createElement('img');
+//   image.classList.add('gallery__image');
+//   image.src = item.preview;
+//   image.alt = item.description;
+
+//   link.appendChild(image);
+//   listItem.appendChild(link);
+//   galleryEl.appendChild(listItem);
+// });
+
 galleryItems.forEach((item) => {
   const listItem = document.createElement('li');
   listItem.classList.add('gallery__item');
 
-  const link = document.createElement('a');
-  link.classList.add('gallery__link');
-  link.href = item.original;
+  listItem.innerHTML = `
+    <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}">
+    </a>
+  `;
 
-  const image = document.createElement('img');
-  image.classList.add('gallery__image');
-  image.src = item.preview;
-  image.alt = item.description;
-
-  link.appendChild(image);
-  listItem.appendChild(link);
   galleryEl.appendChild(listItem);
 });
 
